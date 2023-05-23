bd697fe9a8868b8107833dd34b950b9447f3f63d
DgHung23 <hungdangmcn@gmail.com>
2023-05-23 20:29:46 +0700

    fixbug bai9

--- bai9/code.py
+++ bai9/code.py
@@ -1,18 +1,42 @@
 class Tprime:
-
-    def __init__(self,n):
+    def __init__(self, n):
         self.n = n
+        self.primes = self.generate_prime_numbers(n)
+        
+    def __getitem__(self, k):
+        return self.primes[k]
+        
+    def generate_prime_numbers(self, n):
+        primes = []
+        temp = 0
+        num = 2
+        
+        while temp < n:
+            if self.snt(num):
+                primes.append(num)
+                temp += 1
+            num += 1
+        
+        return primes
+        
+    def snt(self, num):
+        if num < 2:
+            return False
+        
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                return False
+        
+        return True
+
+
 
 pnum = Tprime(int(input("nhap n:")))
 
+for i in range(pnum.n):
+    print("Số nguyên tố thứ", i + 1, "là:", pnum[i])
 
-def snt(test):
-    if test < 2:
-        return False
-    else:
-        for i in range(2, test//2+1):
-            if test % i == 0:
-                return False
-        return True
+
+
 
 
